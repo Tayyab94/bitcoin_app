1e7ee388f15d866470ebaefd5759e52f7b81eda1
tayyab94 <tayyab.bhatti30@gmail.com>
2019-11-07 03:52:42 -0800

    First commit of our Practice project and today we have just built simple Layout of the Project

---
+++ lib/Price_Screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('🤑 Coin Ticker'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(18, 18, 18, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 28),
+                child: Text(
+                  '1 BTC = ? USD',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 150,
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(bottom: 30),
+            color: Colors.lightBlue,
+            child: Text("Tayyab"),
+          ),
+        ],
+      ),
+    );
+  }
+}
